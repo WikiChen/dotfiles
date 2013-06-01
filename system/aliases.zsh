c31885466fea489a5f174a4237aa682174cf63e1
wikichen <wikichen@server.fake>
2013-05-31 19:40:53 -0700

    Edit system aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -6,5 +6,6 @@ then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias la='gls -la --color'
+  alias lsa='gls -A --color'
 fi
